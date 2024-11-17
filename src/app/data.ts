80aab88a1fad566cff23b410efc3089c32294f2f
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-11-17 14:04:53 -0700

    updated prod build

--- src/app/data.ts
+++ src/app/data.ts
@@ -2,7 +2,7 @@ export const CLOSE_ACCESSORIES: Accessory[] = [
     {
         id: 'pink-shades',
         name: 'pink-shades',
-        imageUrl: '/assets/accessories/close/pink-shades.png',
+        imageUrl: '/581-project3/assets/accessories/close/pink-shades.png',
         productUrl: 'https://ca.pitviper.com/products/the-money-counter-original?variant=47756566036756&utm_source=google&utm_medium=cpc&utm_campaign=google_performance_max_non_brand_canada-20938013200&utm_content=&utm_term=&gad_source=1&gclid=Cj0KCQiAouG5BhDBARIsAOc08RS4OLGphIAeZgFw1uZSd26_7S-ZHZV1Vpug---zKFLMGxfKyyzYGMkaAsnUEALw_wcB',
         imageWidth: 200,
         imageHeight: 75,
@@ -11,7 +11,7 @@ export const CLOSE_ACCESSORIES: Accessory[] = [
     {
         id: 'another-chain',
         name: 'another-chain',
-        imageUrl: '/assets/accessories/close/another-chain.png',
+        imageUrl: '/581-project3/assets/accessories/close/another-chain.png',
         productUrl: 'https://www.amazon.ca/Ahier-Necklace-Dollar-Plated-Pendant/dp/B07KB1H4ZB/ref=asc_df_B07KB1H4ZB/?tag=googleshopc0c-20&linkCode=df0&hvadid=706746523378&hvpos=&hvnetw=g&hvrand=2239993414951280512&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9192145&hvtargid=pla-639086874687&mcid=a6a864aa4c3a398cb6bff03c6acf018f&gad_source=1&th=1',
         imageWidth: 300,
         imageHeight: 200,
@@ -20,7 +20,7 @@ export const CLOSE_ACCESSORIES: Accessory[] = [
     {
         id: 'circle-glasses',
         name: 'circle-glasses',
-        imageUrl: '/assets/accessories/close/circle-glasses.png',
+        imageUrl: '/581-project3/assets/accessories/close/circle-glasses.png',
         productUrl: 'https://www.zeelool.com/goods-detail/ZOA01776-01?utm_source=google_shopping&utm_source=google&utm_medium=cpc&utm_campaign=max&utm_campaignId=17246009016&gad_source=1&gclid=Cj0KCQiAouG5BhDBARIsAOc08RQ_wMOJJKuaUsCX6S8ZdmeGuznmYQxWU4yOLJH7keK7rUKTcq4c0GgaArF7EALw_wcB',
         imageWidth: 200,
         imageHeight: 175,
@@ -29,7 +29,7 @@ export const CLOSE_ACCESSORIES: Accessory[] = [
     {
         id: 'glasses',
         name: 'glasses',
-        imageUrl: '/assets/accessories/close/glasses.png',
+        imageUrl: '/581-project3/assets/accessories/close/glasses.png',
         productUrl: 'https://www.clearly.ca/en-ca/glasses/clearly/qy3024-8056262061916?cid=PM-FGS_000000-5.CA-Clearly-EN-NB-NA-GLNewCustomerPMax-NA-NA-NA-Ecom-NA_&gad_source=1&gclid=Cj0KCQiAouG5BhDBARIsAOc08RQHMaPAH1b28RvViCPRg4KyPtef9d8T02sPDG7fCYTrpiUxnTW0P6EaAgBVEALw_wcB&gclsrc=aw.ds',
         imageWidth: 200,
         imageHeight: 175,
@@ -38,7 +38,7 @@ export const CLOSE_ACCESSORIES: Accessory[] = [
     {
         id: 'touque',
         name: 'touque',
-        imageUrl: '/assets/accessories/close/touque.png',
+        imageUrl: '/581-project3/assets/accessories/close/touque.png',
         productUrl: 'https://www.amazon.ca/Connectyle-Classic-Winter-Thick-Beanie/dp/B01LXCBA22/ref=asc_df_B01LXCBA22/?tag=googleshopc0c-20&linkCode=df0&hvadid=706731984180&hvpos=&hvnetw=g&hvrand=16898751257916161164&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9192145&hvtargid=pla-859516085954&psc=1&mcid=c07d8d5eae873d36851093999864ed15&gad_source=1',
         imageWidth: 300,
         imageHeight: 250,
@@ -47,7 +47,7 @@ export const CLOSE_ACCESSORIES: Accessory[] = [
     {
         id: 'necklace',
         name: 'necklace',
-        imageUrl: '/assets/accessories/close/gold-chain.png',
+        imageUrl: '/581-project3/assets/accessories/close/gold-chain.png',
         productUrl: 'https://www.davidyurman.com/ca/en/mens/necklaces-and-chains/fluted-chain-necklace-in-18k-yellow-gold-CH0410M88.html?dwvar_CH0410M88_color=ZZ&dwvar_CH0410M88_size=22%20IN&dwvar_CH0410M88_metal=88&glCountry=CA&glCurrency=CAD&utm_medium=search-paid-pla&utm_source=google&utm_campaign=20220523_ca_evergreen&utm_content=product-feed&&utm_source=google&utm_medium=search-paid-brand&utm_campaign=04502124-evergreen-2h&utm_id=04502124_can&utm_source_platform=sa360&utm_content=04505349_pmax&utm_term=&gad_source=1&gclid=Cj0KCQiAouG5BhDBARIsAOc08RQeL-32AjnfLS3W153rgEvGpAfInqG8t8M6h1OTl3wQpXNLmuCslSgaAtL3EALw_wcB&gclsrc=aw.ds',
         imageWidth: 200,
         imageHeight: 100,
@@ -56,7 +56,7 @@ export const CLOSE_ACCESSORIES: Accessory[] = [
     {
         id: 'cowboyhat',
         name: 'hat',
-        imageUrl: '/assets/accessories/close/hat.png',
+        imageUrl: '/581-project3/assets/accessories/close/hat.png',
         productUrl: 'https://www.temu.com/ul/kuiper/un9.html?subj=goods-un&_bg_fs=1&_p_jump_id=894&_x_vst_scene=adg&goods_id=601099601200132&sku_id=17592535541227&adg_ctx=a-757c0f06~c-d4b8d34e~f-2adc4018&_x_ads_sub_channel=shopping&_p_rfs=1&_x_ns_prz_type=-1&_x_ns_sku_id=17592535541227&_x_ns_gid=601099601200132&mrk_rec=1&_x_ads_channel=google&_x_gmc_account=695390730&_x_login_type=Google&_x_ads_account=3382097441&_x_ads_set=21870955330&_x_ads_id=172213988760&_x_ads_creative_id=719576454436&_x_ns_source=g&_x_ns_gclid=Cj0KCQiAouG5BhDBARIsAOc08RSxxB7aTWHROJOTzBRwgTVm0AZhu1XKR3snLHQviqI0e2HuabGl7yMaAtV8EALw_wcB&_x_ns_placement=&_x_ns_match_type=&_x_ns_ad_position=&_x_ns_product_id=17592535541227&_x_ns_target=&_x_ns_devicemodel=&_x_ns_wbraid=Cj4KCAiA_9u5BhB-Ei4AlGCf7V8aPub7fOKG_k5IRAr1jm95CvNvMCTvE8Kb0ubVU6KUxghjAW-xqeYiGgLg5Q&_x_ns_gbraid=0AAAAAo4mICGcHTdenlE6c46i7P1oTwTjn&_x_ns_targetid=pla-2371221789522&gad_source=1&gclid=Cj0KCQiAouG5BhDBARIsAOc08RSxxB7aTWHROJOTzBRwgTVm0AZhu1XKR3snLHQviqI0e2HuabGl7yMaAtV8EALw_wcB',
         imageWidth: 300,
         imageHeight: 150,
@@ -65,7 +65,7 @@ export const CLOSE_ACCESSORIES: Accessory[] = [
     {
         id: 'scarf',
         name: 'scarf',
-        imageUrl: '/assets/accessories/close/scarf.png',
+        imageUrl: '/581-project3/assets/accessories/close/scarf.png',
         productUrl: 'https://www.amazon.ca/amokk-Winter-Weather-Fashionable-Scarves/dp/B0BHXWJC2M/ref=asc_df_B0BHXWJC2M/?tag=googleshopc0c-20&linkCode=df0&hvadid=706832359184&hvpos=&hvnetw=g&hvrand=18132152202092526848&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9192145&hvtargid=pla-2302000839061&psc=1&mcid=b669defac264316a969df0e8d2964f1b&gad_source=1',
         imageWidth: 250,
         imageHeight: 300,
@@ -77,7 +77,7 @@ export const FAR_ACCESSORIES: Accessory[] = [
     {
         id: 'patagonia-jacket',
         name: 'patagonia-jacket',
-        imageUrl: '/assets/accessories/far/patagonia-jacket.png',
+        imageUrl: '/581-project3/assets/accessories/far/patagonia-jacket.png',
         productUrl: 'https://www.patagonia.ca/product/mens-lightweight-synchilla-snap-t-fleece-pullover/195699299283.html?gad_source=1&gclid=Cj0KCQiAouG5BhDBARIsAOc08RQDMulbWaouuBMpAHKW1Vzt6mkr6-rEEzkIIwBlahHyRD2W0YlwZGMaArQQEALw_wcB',
         imageWidth: 325,
         imageHeight: 275,
@@ -86,7 +86,7 @@ export const FAR_ACCESSORIES: Accessory[] = [
     {
         id: 'flames-jersey',
         name: 'flames-jersey',
-        imageUrl: '/assets/accessories/far/flames-jersey.png',
+        imageUrl: '/581-project3/assets/accessories/far/flames-jersey.png',
         productUrl: 'https://www.sportchek.ca/en/pdp/calgary-flames-adidas-prime-authentic-jersey-hockey-nhl-13945427f.333527816.html?gad_source=1&gclid=Cj0KCQiAouG5BhDBARIsAOc08RQv3a6geFrzMZJH19yefW9E0_ePg4zmRmUpHBnALWo-s_98URbkwrwaAgXbEALw_wcB&gclsrc=aw.ds',
         imageWidth: 325,
         imageHeight: 275,
@@ -95,7 +95,7 @@ export const FAR_ACCESSORIES: Accessory[] = [
     {
         id: 'hawaiian-shirt',
         name: 'hawaiian-shirt',
-        imageUrl: '/assets/accessories/far/hawaiian-shirt.png',
+        imageUrl: '/581-project3/assets/accessories/far/hawaiian-shirt.png',
         productUrl: 'https://www.temu.com/ul/kuiper/un9.html?subj=goods-un&_bg_fs=1&_p_jump_id=894&_x_vst_scene=adg&goods_id=601099627589196&sku_id=17592627961427&adg_ctx=a-8d48b52f~c-4d1c326a~f-2adc4018&_x_ads_sub_channel=shopping&_p_rfs=1&_x_ns_prz_type=-1&_x_ns_sku_id=17592627961427&_x_ns_gid=601099627589196&mrk_rec=1&_x_ads_channel=google&_x_gmc_account=5359365751&_x_login_type=Google&_x_ads_account=7536160690&_x_ads_set=21548916858&_x_ads_id=169294190361&_x_ads_creative_id=708396007403&_x_ns_source=g&_x_ns_gclid=Cj0KCQiAouG5BhDBARIsAOc08RTNdn5WkqpHaravTvQY1h0vpe4q8bJoBa8_j5jX95Gokxb2wV4RmOcaApjiEALw_wcB&_x_ns_placement=&_x_ns_match_type=&_x_ns_ad_position=&_x_ns_product_id=5359365751-17592627961427&_x_ns_target=&_x_ns_devicemodel=&_x_ns_wbraid=Cj4KCAiA_9u5BhB-Ei4AlGCf7VOInM8ExOacM_WdSzzNuF26_jkq2r1recZQVdV-u2hPyprMUTMcV9_iGgIdCw&_x_ns_gbraid=0AAAAAo4mICGZsZfHsNY734_hRqlrSpMQs&_x_ns_targetid=pla-2528173851112&gad_source=1&gclid=Cj0KCQiAouG5BhDBARIsAOc08RTNdn5WkqpHaravTvQY1h0vpe4q8bJoBa8_j5jX95Gokxb2wV4RmOcaApjiEALw_wcB',
         imageWidth: 350,
         imageHeight: 325,
@@ -104,7 +104,7 @@ export const FAR_ACCESSORIES: Accessory[] = [
     {
         id: 'blue-dress',
         name: 'blue-dress',
-        imageUrl: '/assets/accessories/far/blue-dress.png',
+        imageUrl: '/581-project3/assets/accessories/far/blue-dress.png',
         productUrl: 'https://www.amazon.ca/DRESSTELLS-Bridesmaid-Homecoming-Cocktail-RoyalBlue/dp/B07Y32GQYP/ref=asc_df_B07Y32GQYP/?tag=googleshopc0c-20&linkCode=df0&hvadid=706738394100&hvpos=&hvnetw=g&hvrand=15611172707894417360&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9192145&hvtargid=pla-2490722687432&psc=1&mcid=398331ab90393b6890a806b2e60958d2&gad_source=1',
         imageWidth: 425,
         imageHeight: 400,
@@ -113,7 +113,7 @@ export const FAR_ACCESSORIES: Accessory[] = [
     {
         id: 'suit-top',
         name: 'suit-top',
-        imageUrl: '/assets/accessories/far/suit-top.png',
+        imageUrl: '/581-project3/assets/accessories/far/suit-top.png',
         productUrl: 'https://www.mooresclothing.ca/p/joseph-abboud-slim-fit-suit-separate-jacket-msp-33z4-16?CAWELAID=120384850000033579&utm_content=33Z4_16-36%20Regular-Charcoal%20Tic&gad_source=1&gclid=Cj0KCQiAouG5BhDBARIsAOc08RQduoJPK0l44apqbYfNtf16zYJqiXvt7DfhE914iBdhJmHb4SEqnXMaAm9nEALw_wcB#colour=charcoal-tic%26size=36%26length=regular%26',
         imageWidth: 250,
         imageHeight: 375,
@@ -122,7 +122,7 @@ export const FAR_ACCESSORIES: Accessory[] = [
     {
         id: 'crewneck',
         name: 'crewneck',
-        imageUrl: '/assets/accessories/far/crewneck.png',
+        imageUrl: '/581-project3/assets/accessories/far/crewneck.png',
         productUrl: 'https://arcteryx.com/ca/en/shop/mens/emblem-fleece-crew-neck-pullover-9787?cmpid=pm|mult|perf|google|Arc%27teryx_Google-PMAX_Non-Brand_S23_Performance_BOF_R:NAM_C:CA_L:EN&utm_source=google&utm_medium=pm|mult|perf&utm_campaign=Arc%27teryx_Google-PMAX_Non-Brand_S23_Performance_BOF_R:NAM_C:CA_L:EN&gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQiAouG5BhDBARIsAOc08RSGfnRsdbqnrVwpgQs5w-t65rfkkMgqsZQgmMmBs5g3Yb9cLjk4-9EaAhp0EALw_wcB',
         imageWidth: 325,
         imageHeight: 275,
@@ -131,7 +131,7 @@ export const FAR_ACCESSORIES: Accessory[] = [
     {
         id: 'button-up',
         name: 'button-up',
-        imageUrl: '/assets/accessories/far/button-up.png',
+        imageUrl: '/581-project3/assets/accessories/far/button-up.png',
         productUrl: 'https://www.zara.com/ca/en/plaid-shirt-p03067125.html?v1=395477136&utm_source=google&utm_medium=cpc&gad_source=1&gclid=Cj0KCQiAouG5BhDBARIsAOc08RSo-li8gbdk-BzbSgH8s3Y36w0wQzDKpcRR4_xNXIsn0MVZoRYCsCMaAhweEALw_wcB',
         imageWidth: 325,
         imageHeight: 275,
